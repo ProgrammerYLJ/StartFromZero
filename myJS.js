e9080c23f0ead7ca6ef009543da91edae6f40782
霖 <peter.jiang96@gmail.com>
2017-07-29 18:32:33 +0800

    FIX BUGS

--- myJS.js
+++ myJS.js
@@ -1,28 +1,26 @@
 function computeAP(){
 	
 	var now = new Date();
-	var years = now.getYear();
-	var months = now.getMonth()+1;
-	var days = now.getDate();
-	var hours = now.getHours();
-	var minutes = now.getMinutes();
 	
 	var nowAP = +document.getElementById('nowAP').value;
 	var restTime = document.getElementById('restTime').value;
 	
 	var restTimeAP_obj = document.getElementById('restTimeAP');
 	
-	if(restTime > hours)
+	var restTimeArr = restTime.split(':');
+	
+	if(restTimeArr[0] > now.getHours() || (restTimeArr[0] == now.getHours() && restTimeArr[1] > now.getMinutes()))
 	{
-		var Date_B = new Date(years,months-1,days,restTime,0,0);  
-		var Date_C = new Date(Date_B - now);
-		var AP_Vol = Math.floor((Date_C.getUTCHours()*60 + Date_C.getUTCMinutes())/5) + nowAP;
-		restTimeAP_obj.value = AP_Vol;
+			var Date_B = new Date(now.getYear(),now.getMonth(),now.getDate(),restTimeArr[0],restTimeArr[1],0);  
+			var Date_C = new Date(Date_B - now);
+			var AP_Vol = Math.floor((Date_C.getUTCHours()*60 + Date_C.getUTCMinutes())/5) + nowAP;
+			restTimeAP_obj.value = AP_Vol;	
+	}
+	else
+	{
+		restTimeAP_obj.value ="Error!";
 	}
 	
 }
 
-function focusON(){
-	alert(333);
-	//document.getElementById('nowAP').focus();
-}
+
