737eabd96d1bc4e6ce31a9ac5b3751aa22076bc6
霖 <peter.jiang96@gmail.com>
2017-07-29 16:42:21 +0800

    Add FGO AP calculator
    calculate by hour

--- myJS.js
+++ myJS.js
@@ -1,1 +1,23 @@
 
+function computeAP(){
+	
+	
+	var now = new Date();
+	var years = now.getYear();
+	var months = now.getMonth()+1;
+	var days = now.getDate();
+	var hours = now.getHours();
+	var minutes = now.getMinutes();
+	
+	var nowAP = +document.getElementById('nowAP').value;
+	var restTime = document.getElementById('restTime').value;
+	
+	var restTimeAP_obj = document.getElementById('restTimeAP');
+	
+	if(restTime > hours){
+		
+		var a = 12*(restTime-hours) + nowAP;
+		restTimeAP_obj.value=a;
+	}
+	
+}
